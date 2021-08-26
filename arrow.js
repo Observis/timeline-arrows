f1995ed6085267bade8c5931b993af3e0bad9733
Joonatan Rantanen <joonatan.rantanen@observis.fi>
2021-08-26 16:29:21 +0300

    initial changes to arrow removing and drawing

--- arrow.js
+++ arrow.js
@@ -148,37 +148,46 @@ export default class Arrow {
             }
         }
 
-        if ( (groupOf_1_isVisible && groupOf_2_isVisible) && (oneItemVisible) && (bothItemsExist)) {
-            var item_1 = this._getItemPos(this._timeline.itemSet.items[dep.id_item_1]);
-            var item_2 = this._getItemPos(this._timeline.itemSet.items[dep.id_item_2]);
-            if (item_2.mid_x < item_1.mid_x) [item_1, item_2] = [item_2, item_1]; // As demo, we put an arrow between item 0 and item1, from the one that is more on left to the one more on right.
-            var curveLen = item_1.height * 2; // Length of straight Bezier segment out of the item.
-            item_2.left -= 10; // Space for the arrowhead.
-            this._dependencyPath[index].setAttribute("marker-end", "url(#arrowhead0)");
-            this._dependencyPath[index].setAttribute(
-            "d",
-            "M " +
-                item_1.right +
-                " " +
-                item_1.mid_y +
-                " C " +
-                (item_1.right + curveLen) +
-                " " +
-                item_1.mid_y +
-                " " +
-                (item_2.left - curveLen) +
-                " " +
-                item_2.mid_y +
-                " " +
-                item_2.left +
-                " " +
-                item_2.mid_y
-            );
-            // Adding the title if property title has been added in the dependency
-            if (dep.hasOwnProperty("title")) {
-                this._dependencyPath[index].innerHTML = "<title>" +dep.title +"</title>"
+        if ( (groupOf_1_isVisible && groupOf_2_isVisible) && (bothItemsExist)) {
+          var item_1 = this._getItemPos(this._timeline.itemSet.items[dep.id_item_1]);
+          var item_2 = this._getItemPos(this._timeline.itemSet.items[dep.id_item_2]);
+          var curveLen = item_1.height * 2; // Length of straight Bezier segment out of the item. 
+          
+          if (Math.abs(item_1.mid_y - item_2.mid_y) < item_1.height) {
+            curveLen = 0;
+
+            if (Math.abs(item_1.right - item_2.left) < 10) {
+              this._dependencyPath[index].setAttribute("marker-end", "");
+              this._dependencyPath[index].setAttribute("d", "M 0 0");
+              return;
             }
-        } else {
+          }
+          item_2.left -= 10; // Space for the arrowhead.
+          this._dependencyPath[index].setAttribute("marker-end", "url(#arrowhead0)");
+          this._dependencyPath[index].setAttribute(
+          "d",
+          "M " +
+              item_1.right +
+              " " +
+              item_1.mid_y +
+              " C " +
+              (item_1.right + curveLen) +
+              " " +
+              item_1.mid_y +
+              " " +
+              (item_2.left - curveLen) +
+              " " +
+              item_2.mid_y +
+              " " +
+              item_2.left +
+              " " +
+              item_2.mid_y
+          );
+          // Adding the title if property title has been added in the dependency
+          if (dep.hasOwnProperty("title")) {
+              this._dependencyPath[index].innerHTML = "<title>" +dep.title +"</title>"
+          }
+      } else {
             this._dependencyPath[index].setAttribute("marker-end", "");
             this._dependencyPath[index].setAttribute("d", "M 0 0");
         }
@@ -186,19 +195,26 @@ export default class Arrow {
     }
 
     //Función que recibe in Item y devuelve la posición en pantalla del item.
-    _getItemPos (item) {
-        let left_x = item.left;
-        let top_y = item.parent.top + item.parent.height - item.top - item.height;
-        return {
-            left: left_x,
-            top: top_y,
-            right: left_x + item.width,
-            bottom: top_y + item.height,
-            mid_x: left_x + item.width / 2,
-            mid_y: top_y + item.height / 2,
-            width: item.width,
-            height: item.height
-        }
+    _getItemPos (item) { 
+      let left_x, right_x;
+      if (item.conversion && item.conversion.toScreen) {
+        left_x = item.conversion.toScreen(item.data.start);
+        right_x = item.conversion.toScreen(item.data.end);
+      } else {
+        left_x = item.left;
+        right_x = left_x + item.width;
+      }
+      let top_y = item.parent.top + item.top;
+      return {
+          left: left_x,
+          top: top_y,
+          right: right_x,
+          bottom: top_y + item.height,
+          mid_x: left_x + item.width / 2,
+          mid_y: top_y + item.height / 2,
+          width: item.width,
+          height: item.height
+      }
     }
 
 
@@ -218,18 +234,13 @@ export default class Arrow {
     }
     
     //Función que recibe el id de una flecha y la elimina.
-    removeArrow(id) {
-        for (let i = 0; i < this._dependency.length; i++) {
-            if (this._dependency[i].id == id) var index = i;
-        }
-
-        //var list = document.getElementsByTagName("path"); //FALTA QUE ESTA SELECCION LA HAGA PARA EL DOM DEL TIMELINE INSTANCIADO!!!!
-        var list = document.querySelectorAll("#" +this._timeline.dom.container.id +" path");
-
-        this._dependency.splice(index, 1); //Elimino del array dependency
-        this._dependencyPath.splice(index, 1); //Elimino del array dependencyPath
-        
-        list[index + 1].parentNode.removeChild(list[index + 1]); //Lo elimino del dom
+    removeArrow(id) { 
+      for (let i = 0; i < this._dependency.length; i++) {
+          if (this._dependency[i].id == id) var index = i;
+      }
+      this._svg.removeChild(this._dependencyPath[index]);
+      this._dependency.splice(index, 1); 
+      this._dependencyPath.splice(index, 1); 
     }
 
     //Función que recibe el id de un item y elimina la flecha.
